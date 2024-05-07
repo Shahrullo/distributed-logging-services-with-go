a02800be8a98001b6b6c99765dbac627e9189502
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo@laivdata.com>
2024-05-07 13:48:37 +0900

    gRPC resolver functionality is added

---
+++ internal/loadbalance/resolver.go
@@ -0,0 +1,56 @@
+package loadbalance
+
+import (
+	"fmt"
+	"sync"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/serviceconfig"
+)
+
+type Resolver struct {
+	mu            sync.Mutex
+	clientConn    resolver.ClientConn
+	resolverConn  *grpc.ClientConn
+	serviceConfig *serviceconfig.ParseResult
+	logger        *zap.Logger
+}
+
+var _ resolver.Builder = (*Resolver)(nil)
+
+func (r *Resolver) Build(
+	target resolver.target,
+	cc resolver.ClientConn,
+	opts resolver.BuildOptions,
+) (resolver.Resolver, error) {
+	r.logger = zap.L().Named("resolver")
+	r.clientConn = cc
+	var dialOpts []grpc.dialOpts
+	if opts.DialCreds != nil {
+		dialOpts = append(
+			dialOpts,
+			grpc.WithTransportCredentials(opts.DialCreds),
+		)
+	}
+	r.serviceConfig = &r.clientConn.ParseServiceConfig(
+		fmt.Sprintf(`{"loadBalancingConfig":[{%s":{}}]}`, Name),
+	)
+	var err error
+	r.resolverConn, err = grpc.Dial(target.Endpoint, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	r.ResolveNow(resolver.ResolveNowOptions{})
+}
+
+const Name = "proglog"
+
+func (r *Resolver) Scheme() string {
+	return Name
+}
+
+func init() {
+	resolver.Register(&Resolver{})
+}
